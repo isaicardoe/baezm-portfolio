cb4647d1452552853d331803e521f8a03277e83d
isai cardoso <isai.cardoe@gmail.com>
2014-09-08 09:08:32 -0700

    updating

--- public_html/js/jsstuff.js
+++ public_html/js/jsstuff.js
@@ -1,57 +1,30 @@
-function scrollFooter(scrollY, heightFooter)
-{
-    console.log(scrollY);
-    console.log(heightFooter);
-
-    if(scrollY >= heightFooter)
-    {
-        $('footer').css({
-            'bottom' : '0px'
-        });
-    }
-    else
-    {
-        $('footer').css({
-            'bottom' : '-' + heightFooter + 'px'
-        });
-    }
-}
-
-$(window).load(function(){
-    var windowHeight        = $(window).height(),
-        footerHeight        = $('footer').height(),
-        heightDocument      = (windowHeight) + ($('.content').height()) + ($('footer').height()) - 20;
-
-    // Definindo o tamanho do elemento pra animar
-    $('#scroll-animate, #scroll-animate-main').css({
-        'height' :  heightDocument + 'px'
-    });
-
-    // Definindo o tamanho dos elementos header e conteúdo
-    $('header').css({
-        'height' : windowHeight + 'px',
-        'line-height' : windowHeight + 'px'
-    });
-
-    $('.wrapper-parallax').css({
-        'margin-top' : windowHeight + 'px'
-    });
-
-    scrollFooter(window.scrollY, footerHeight);
-
-    // ao dar rolagem
-    window.onscroll = function(){
-        var scroll = window.scrollY;
-
-        $('#scroll-animate-main').css({
-            'top' : '-' + scroll + 'px'
-        });
-        
-        $('header').css({
-            'background-position-y' : 50 - (scroll * 100 / heightDocument) + '%'
-        });
-
-        scrollFooter(scroll, footerHeight);
-    }
+$(document).ready(function() {
+  
+  // On click, remove class on active element, add it to the new one
+  
+  $('header nav a').click(function(e) {
+    
+    $('header nav a.active').removeClass('active');
+    $(this).addClass('active');
+    
+    // Scroll to anchor
+    
+    $('html,body').animate({scrollTop: $($(this).attr('href')).offset().top - 70},'slow');
+    
+    e.preventDefault();
+    return false;
+    
+  });
+  
+  // On scroll, remove class on active element and add it to the new one
+  
+  $(document).scroll(function() {
+     
+     var position = Math.floor($(this).scrollTop() / 800) + 1;
+    
+     $('header nav a.active').removeClass('active');
+     $('header nav a.link-' + position).addClass('active');
+    
+  });
+  
 });
-
