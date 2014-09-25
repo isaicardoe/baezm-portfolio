b3754b842ca20b5f808d9f0e2f57770c82740091
isai cardoso <isai.cardoe@gmail.com>
2014-09-25 12:07:20 -0700

    styles

--- public_html/js/jsstuff.js
+++ public_html/js/jsstuff.js
@@ -33,33 +33,3 @@ $(document).ready(function() {
 	 
 });
 
-$(document).ready(function(){
-	 function scrollNav() {
-	  $('nav a').click(function(){  
-	    //Toggle Class
-	    $(".active-link").removeClass("active-link");      
-	    $(this).closest('li').addClass("active-link");
-	    var theClass = $(this).attr("class");
-	    $('.'+theClass).parent('li').addClass('active-link');
-	    //Animate
-	    $('html, body').stop().animate({
-	        scrollTop: $( $(this).attr('href') ).offset().top - 160
-	    }, 400);
-	    return false;
-	  });
-	  $('.scrollTop a').scrollTop();
-	}
-	scrollNav();
-})
-
-$(document).scroll(function(){
-  if($(this).scrollTop() > 355) {
-    $('.top').css({
-      'opacity':'1',
-    });
-  } else {
-    $('.top').css({
-      'opacity':'0',
-    });
-  }
-})
